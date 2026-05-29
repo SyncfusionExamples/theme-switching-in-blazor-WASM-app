35518a797906161cc5c83ab0e52dceff4c82157c
VaseegaranSF4468 <162665726+VaseegaranSF4468@users.noreply.github.com>
2026-05-29 13:02:27 +0530

    1028569: Blazor WASM app theme switching in .NET 10

---
+++ NET10/BlazorApp/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorApp;
+using Syncfusion.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddSyncfusionBlazor();
+
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
